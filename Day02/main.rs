4508377128748089aab72f80e45c735128e693ae
LJmartin94 <lindsayjamesmartin94@gmail.com>
2022-12-02 16:09:25 +0100

    Same implementation, just changing some symbols around.

--- Day02/main.rs
+++ Day02/main.rs
@@ -1,6 +1,6 @@
 fn main()
 {
-	let file_name = "./input.txt";
+	let file_name = "./test_input.txt";
 	let input = std::fs::read_to_string(file_name).expect("Wasn't able to read input");
 	let str_input_vector: Vec <&str> = input.lines().collect();
 
@@ -28,7 +28,7 @@ fn main()
 	loss_vec.push(op_scissors.to_string() + " " + &symbol_paper.to_string());
 
 	let mut total_score = 0;
-	for s in str_input_vector
+	for s in &str_input_vector
 	{
 		if s.chars().last().unwrap() == symbol_rock{
 			total_score += 1;
@@ -50,5 +50,52 @@ fn main()
 			total_score += 0;
 		}
 	}
-	println!("Total score: {}\n", total_score);
+	println!("Total score (first puzzle): {}\n", total_score);
+
+	let symbol_loss = 'X'; // 0
+	let symbol_draw = 'Y'; // 3
+	let symbol_win = 'Z'; //6
+
+	let mut rock_vec = Vec::new();
+	rock_vec.push(op_rock.to_string() + " " + &symbol_draw.to_string());
+	rock_vec.push(op_paper.to_string() + " " + &symbol_loss.to_string());
+	rock_vec.push(op_scissors.to_string() + " " + &symbol_win.to_string());
+
+	let mut paper_vec = Vec::new();
+	paper_vec.push(op_rock.to_string() + " " + &symbol_win.to_string());
+	paper_vec.push(op_paper.to_string() + " " + &symbol_draw.to_string());
+	paper_vec.push(op_scissors.to_string() + " " + &symbol_loss.to_string());
+
+	let mut scissor_vec = Vec::new();
+	scissor_vec.push(op_rock.to_string() + " " + &symbol_loss.to_string());
+	scissor_vec.push(op_paper.to_string() + " " + &symbol_win.to_string());
+	scissor_vec.push(op_scissors.to_string() + " " + &symbol_draw.to_string());
+
+	total_score = 0;
+	for s in &str_input_vector
+	{
+		if s.chars().last().unwrap() == symbol_loss{
+			total_score += 0;
+		}
+		else if s.chars().last().unwrap() == symbol_draw{
+			total_score += 3;
+		}
+		else if s.chars().last().unwrap() == symbol_win{
+			total_score += 6;
+		}
+
+		if rock_vec.iter().any(|i| i == s){
+			total_score += 1;
+		}
+		else if paper_vec.iter().any(|i| i == s){
+			total_score += 2;
+		}
+		else if scissor_vec.iter().any(|i| i == s){
+			total_score += 3;
+		}
+	}
+
+	println!("Total score (second puzzle): {}\n", total_score);
+
+
 }
